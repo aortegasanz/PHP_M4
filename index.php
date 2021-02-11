2c9f180d2376e5bc3fea8ce16f1fda739f7a5f7e
Alberto Ortega <ortegasanz@gmail.com>
2021-02-11 17:04:57 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP_M4 Pr&aacute;ctica Bootstrap </title>
+        <link rel="shortcut icon" type="image/x-icon" href="images/ico.ico" />
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Hoja de Estilos - CSS -->        
+        <link rel="stylesheet" href="styles/css/stripes.css">
+        <!-- Fonts Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <!-- Google Fonts (LATO) -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400&display=swap" rel="stylesheet">        
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+        <main>
+        
+        </main>
+    </body>
+</html>
